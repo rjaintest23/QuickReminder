cf7824bd77d1e1c5e3708e2f2c0ca1cb8b6596d2
rjaintest23 <139117662+rjaintest23@users.noreply.github.com>
2025-03-25 23:06:15 +0530

    My First Commit

---
+++ QuickReminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickReminder
+//
+//  Created by Loktantram on 25/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickReminder/QuickReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickReminderApp.swift
+//  QuickReminder
+//
+//  Created by Loktantram on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
